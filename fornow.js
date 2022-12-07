2b37ce3286ce43a88b7cc259c05e99009704bb1f
“Shinsaku <polorll.1221@gmail.com>
2022-12-07 19:09:57 +0900

    add a function to play a round of the game and return a comment according to the result

--- fornow.js
+++ fornow.js
@@ -1,6 +1,7 @@
+let numbers= Math.random() * 10;
 
 function getComputerChoice (){
-    let numbers= Math.random() * 10;
+
     if (4 > numbers && numbers > 0){
         result = "paper";
 
@@ -12,8 +13,33 @@ function getComputerChoice (){
     }
     
     return result;
-     
-    
+
 }
 
 console.log (getComputerChoice());
+console.log (numbers);
+
+
+let playerSelection = prompt("Rock, Paper, Scissors,,,Shoot!", "Type Rock, Paper, or Scissors");
+
+let computerSelection = getComputerChoice();
+
+if (computerSelection == "paper" && playerSelection === "Scissors") {
+    alert ("You win!! Scissors beat paper.");
+} else if (computerSelection == "scissors" && playerSelection === "Rock"){
+    alert ("You win!! Rock beat scissors.");
+} else if (computerSelection == "rock" && playerSelection === "Paper"){
+    alert ("You win!! Paper beat rock.");
+
+} else if (computerSelection == "scissors" && playerSelection === "Paper"){
+    alert ("....... Paper can't beat scissors.");
+} else if (computerSelection == "paper" && playerSelection === "Rock"){
+    alert ("....... Rock can't beat paper.");
+} else if (computerSelection == "rock" && playerSelection === "Scissors"){
+    alert ("....... Scissors can't beat rock.");
+}
+ else {
+    alert ("Draw. Try again!!");
+}
+
+
