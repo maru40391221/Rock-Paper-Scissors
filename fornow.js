6b747bd059056f2b5de1f32fedb15b01e4258d5a
“Shinsaku <polorll.1221@gmail.com>
2022-12-09 21:04:25 +0900

    made prompt input case-insensitive.

--- fornow.js
+++ fornow.js
@@ -1,6 +1,7 @@
-let numbers= Math.random() * 10;
+
 
 function getComputerChoice (){
+    let numbers= Math.random() * 10;
 
     if (4 > numbers && numbers > 0){
         result = "paper";
@@ -17,29 +18,31 @@ function getComputerChoice (){
 }
 
 console.log (getComputerChoice());
-console.log (numbers);
-
-
-let playerSelection = prompt("Rock, Paper, Scissors,,,Shoot!", "Type Rock, Paper, or Scissors");
-
 let computerSelection = getComputerChoice();
 
-if (computerSelection == "paper" && playerSelection === "Scissors") {
+let playerChoice = prompt("Rock, Paper, Scissors,,,Shoot!", "Type Rock, Paper, or Scissors").toLowerCase();
+
+console.log (playerChoice);
+
+if (computerSelection == "paper" && playerChoice == "scissors") {
     alert ("You win!! Scissors beat paper.");
-} else if (computerSelection == "scissors" && playerSelection === "Rock"){
+
+} else if (computerSelection == "scissors" && playerChoice == "rock"){
     alert ("You win!! Rock beat scissors.");
-} else if (computerSelection == "rock" && playerSelection === "Paper"){
+
+} else if (computerSelection == "rock" && playerChoice == "paper"){
     alert ("You win!! Paper beat rock.");
 
-} else if (computerSelection == "scissors" && playerSelection === "Paper"){
+} else if (computerSelection == "scissors" && playerChoice == "paper"){
     alert ("....... Paper can't beat scissors.");
-} else if (computerSelection == "paper" && playerSelection === "Rock"){
+
+} else if (computerSelection == "paper" && playerChoice == "rock"){
     alert ("....... Rock can't beat paper.");
-} else if (computerSelection == "rock" && playerSelection === "Scissors"){
+
+} else if (computerSelection == "rock" && playerChoice == "scissors"){
     alert ("....... Scissors can't beat rock.");
 }
  else {
     alert ("Draw. Try again!!");
 }
-
 
