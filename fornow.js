1ef14bfb97ca2916bc537c8f0ed6369c01e77d9d
“Shinsaku <polorll.1221@gmail.com>
2022-12-10 11:03:09 +0900

    writing for loops to play 5 rounds without hitting the refresh button

--- fornow.js
+++ fornow.js
@@ -1,5 +1,4 @@
 
-
 function getComputerChoice (){
     let numbers= Math.random() * 10;
 
@@ -14,38 +13,49 @@ function getComputerChoice (){
     }
     
     return result;
-
-
 }
-
 
 let computerSelection = getComputerChoice();
-
 console.log (computerSelection);
 
-let playerChoice = prompt("Rock, Paper, Scissors,,,Shoot!", "Type Rock, Paper, or Scissors").toLowerCase();
+function playRound (){
+    let playerChoice = prompt("Rock, Paper, Scissors,,,Shoot!", "Type Rock, Paper, or Scissors").toLowerCase();
+        console.log (playerChoice);
 
-console.log (playerChoice);
+    if (computerSelection == "paper" && playerChoice == "scissors") {
+        alert ("You win!! Scissors beat paper.");
 
-if (computerSelection == "paper" && playerChoice == "scissors") {
-    alert ("You win!! Scissors beat paper.");
+    } else if (computerSelection == "scissors" && playerChoice == "rock"){
+        alert ("You win!! Rock beat scissors.");
 
-} else if (computerSelection == "scissors" && playerChoice == "rock"){
-    alert ("You win!! Rock beat scissors.");
+    } else if (computerSelection == "rock" && playerChoice == "paper"){
+        alert ("You win!! Paper beat rock.");
 
-} else if (computerSelection == "rock" && playerChoice == "paper"){
-    alert ("You win!! Paper beat rock.");
+    } else if (computerSelection == "scissors" && playerChoice == "paper"){
+        alert ("....... Paper can't beat scissors.");
 
-} else if (computerSelection == "scissors" && playerChoice == "paper"){
-    alert ("....... Paper can't beat scissors.");
+    } else if (computerSelection == "paper" && playerChoice == "rock"){
+        alert ("....... Rock can't beat paper.");
 
-} else if (computerSelection == "paper" && playerChoice == "rock"){
-    alert ("....... Rock can't beat paper.");
-
-} else if (computerSelection == "rock" && playerChoice == "scissors"){
-    alert ("....... Scissors can't beat rock.");
+    } else if (computerSelection == "rock" && playerChoice == "scissors"){
+        alert ("....... Scissors can't beat rock.");
+    }
+    else {
+        alert ("Draw. Try again!!");
+    }
 }
- else {
-    alert ("Draw. Try again!!");
+
+
+function game (){
+    for (let i = 0; i < 5; i++) {
+        if (i===0){
+            console.log ("please"))
+            playRound()
+            
+        }
+
+
+     }
 }
 
+game ();
