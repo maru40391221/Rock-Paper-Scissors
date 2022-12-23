3c3a53b0f53a365e0607e95bc7201992f569b876
“Shinsaku <polorll.1221@gmail.com>
2022-12-23 20:10:34 +0900

    add score keeping function and offiially done with a part of ODIN projectgit add .

--- fornow.js
+++ fornow.js
@@ -26,32 +26,40 @@ function computer (){
     computerSelection = computerResult ();
 }
 
+let theNumberOfWin = 0; 
+let theNumberOfLose= 0;
+
 function player (){
     playerChoice = prompt("Rock, Paper, Scissors,,,Shoot!", "Type Rock, Paper, or Scissors").toLowerCase();
     console.log (playerChoice);
 
 if (computerSelection == "paper" && playerChoice == "scissors") {
     alert ("You win!! Scissors beat paper.");
+    theNumberOfWin++;
 
 } else if (computerSelection == "scissors" && playerChoice == "rock"){
     alert ("You win!! Rock beat scissors.");
+    theNumberOfWin++;
 
 } else if (computerSelection == "rock" && playerChoice == "paper"){
     alert ("You win!! Paper beat rock.");
+    theNumberOfWin++;
 
 } else if (computerSelection == "scissors" && playerChoice == "paper"){
     alert ("....... Paper can't beat scissors.");
+    theNumberOfLose++;
 
 } else if (computerSelection == "paper" && playerChoice == "rock"){
     alert ("....... Rock can't beat paper.");
+    theNumberOfLose++;
 
 } else if (computerSelection == "rock" && playerChoice == "scissors"){
     alert ("....... Scissors can't beat rock.");
+    theNumberOfLose++;
 }
 else {
     alert ("Draw. Try again!!");
 }
-
 }
 
 function round (){
@@ -59,91 +67,33 @@ function round (){
     player();
 }
 
-let theNumberOfWin = 0; 
-let theNumberOfLose = 0;
-
-function score (){
-    for (let z = 0; z <= 5; z++){
-        if (z = 0 && (computerSelection == "paper" && playerChoice == "scissors")||
-    (computerSelection == "scissors" && playerChoice == "rock") ||
-    (computerSelection == "rock" && playerChoice == "paper")
-    ) {
-        theNumberOfWin++;
-        
-        return theNumberOfWin;
-    }
-    if (z = 1 && (computerSelection == "paper" && playerChoice == "scissors")||
-    (computerSelection == "scissors" && playerChoice == "rock") ||
-    (computerSelection == "rock" && playerChoice == "paper")
-    ) {
-        theNumberOfWin++;
-        
-        return theNumberOfWin;
-    }
-    if (z = 2 && (computerSelection == "paper" && playerChoice == "scissors")||
-    (computerSelection == "scissors" && playerChoice == "rock") ||
-    (computerSelection == "rock" && playerChoice == "paper")
-    ) {
-        theNumberOfWin++;
-        
-        return theNumberOfWin;
-    }
-    if (z = 3 && (computerSelection == "paper" && playerChoice == "scissors")||
-    (computerSelection == "scissors" && playerChoice == "rock") ||
-    (computerSelection == "rock" && playerChoice == "paper")
-    ) {
-        theNumberOfWin++;
-        
-        return theNumberOfWin;
-    }
-    if (z = 4 && (computerSelection == "paper" && playerChoice == "scissors")||
-    (computerSelection == "scissors" && playerChoice == "rock") ||
-    (computerSelection == "rock" && playerChoice == "paper")
-    ) {
-        theNumberOfWin++;
-        
-        return theNumberOfWin;
-    }
-    if (z = 5 && (computerSelection == "paper" && playerChoice == "scissors")||
-    (computerSelection == "scissors" && playerChoice == "rock") ||
-    (computerSelection == "rock" && playerChoice == "paper")
-    ) {
-        theNumberOfWin++;
-        
-        return theNumberOfWin;
-    }
-}
-}
-
-    
-
 function game (){
     for (let i = 0; i <= 5; i++) {
         if (i===0){
             console.log ("round 1" );
             round();
-            score();
-            console.log (theNumberOfWin);
+            console.log (`Your score ${theNumberOfWin}`);
+            console.log (`Computer's score ${theNumberOfLose}`);
         } else if (i===1){
             console.log ("round 2");
             round();
-            score();
-            console.log (theNumberOfWin);
+            console.log (`Your score ${theNumberOfWin}`);
+            console.log (`Computer's score ${theNumberOfLose}`);
         } else if (i===2){
             console.log ("round 3");
             round();
-            score();
-            console.log (theNumberOfWin);
+            console.log (`Your score ${theNumberOfWin}`);
+            console.log (`Computer's score ${theNumberOfLose}`);
         } else if (i===3){
             console.log ("round 4");
             round();
-            score();
-            console.log (theNumberOfWin);
+            console.log (`Your score ${theNumberOfWin}`);
+            console.log (`Computer's score ${theNumberOfLose}`);
         }else if (i===4){
             console.log ("round 5");
             round();
-            score();
-            console.log (theNumberOfWin);
+            console.log (`Your score ${theNumberOfWin}`);
+            console.log (`Computer's score ${theNumberOfLose}`);
         }else if (i===5){
             console.log ("done")
         }
@@ -152,6 +102,14 @@ function game (){
 
 game ();
 
+function whoIsTheWinner (){
+    if (theNumberOfWin > theNumberOfLose){
+        console.log("You Won!!!");
+    } else if (theNumberOfWin < theNumberOfLose) {
+        console.log("Don't worry about it");
+    } else {
+        console.log ("try again");
+    }
+}
 
-
-/* add a functionality to keep score tomorrow*/
+whoIsTheWinner ();
---
+++ practice.js
@@ -0,0 +1,25 @@
+let apple;
+let orange;
+
+for (let i = 0; i <= 5; i++){
+    if (i=0) {
+        apple;
+    } else if (i=1) {
+        apple++;
+    }else if (i=2) {
+        apple++;
+    }else if (i=3) {
+        apple++;
+    }else if (i=4) {
+        apple++;
+    }else if (i=5) {
+        apple++;
+    }
+        else {
+        orange++;
+    }
+    return apple;
+}
+
+
+console.log (apple,orange);
